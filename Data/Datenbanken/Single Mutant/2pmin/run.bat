c7f4c9bd4653ebb1b8e46e9f1c3038fe404e911c
sholzer <steffenbholzer@googlemail.com>
2016-08-31 15:34:29 +0200

    Änderungen

--- Data/Datenbanken/Single Mutant/2pmin/run.bat
+++
@@ -1,3 +0,0 @@
-del *.db
-del log.txt
-java -Dorg.slf4j.simpleLogger.showShortLogName="true" -Dorg.slf4j.simpleLogger.logFile="./log.txt" -Dcfg="./spring-config.xml" -jar ..\..\parking.simulation-0.0.1-SNAPSHOT-jar-with-dependencies.jar
